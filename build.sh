34cc9f966e2e278260aea68e139e8cb840e7c5a5
Ilya Zuyev <izuyev@google.com>
2020-10-20 16:40:55 -0700

    run minikube

--- build.sh
+++ build.sh
@@ -5,6 +5,14 @@ docker ps
 docker version
 env
 
+curl -LO https://github.com/kubernetes/minikube/releases/download/v1.14.0/minikube-darwin-amd64
+chmod +x minikube-darwin-amd64
+./minikube-darwin-amd64 start --driver=docker
+sleep 60
+./minikube-darwin-amd64 status
+kubectl get pods
+env
+
 set -e
 
 if [ "$1" == "release" ]; then
