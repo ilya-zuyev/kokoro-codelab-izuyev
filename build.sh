b0305db5238ba423b06b4c300086f0035e24c6a4
Ilya Zuyev <izuyev@google.com>
2020-10-20 17:01:51 -0700

    deploy ngnix & check

--- build.sh
+++ build.sh
@@ -5,13 +5,27 @@ docker ps
 docker version
 env
 
-curl -LO https://github.com/kubernetes/minikube/releases/download/v1.14.0/minikube-darwin-amd64
-chmod +x minikube-darwin-amd64
-./minikube-darwin-amd64 start --driver=docker
-sleep 60
-./minikube-darwin-amd64 status
+echo "============================="
+
+curl -LO https://github.com/kubernetes/minikube/releases/download/v1.14.0/minikube-darwin-amd64 -o minikube
+chmod +x minikube
+./minikube start --driver=docker
+./minikube status
 kubectl get pods
-env
+
+env | sort 
+echo "============================="
+eval $(./minikube docker-env)
+
+env | sort 
+
+echo "============================="
+kubectl apply -f https://k8s.io/examples/controllers/nginx-deployment.yaml
+sleep 60
+
+kubectl port-forward deployment/nginx-deployment 7777:80 &
+curl localhost:7777
+kubectl delete deploy --all
 
 set -e
 
