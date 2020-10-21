b104362346ec5565511dbf3fe8b42b04172e4a3f
Ilya Zuyev <izuyev@google.com>
2020-10-21 13:18:52 -0700

    print meminfo

--- build.sh
+++ build.sh
@@ -5,6 +5,10 @@ echo "==== env before installing minikube =="
 env | sort
 echo "============================="
 
+echo "==== memory =="
+cat /proc/meminfo
+echo "============================="
+
 curl -LO https://github.com/kubernetes/minikube/releases/download/v1.14.0/minikube-darwin-amd64
 MK=./minikube-darwin-amd64 
 chmod +x ${MK}
