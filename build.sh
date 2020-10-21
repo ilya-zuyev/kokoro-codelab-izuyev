108702a36041275d96f7a3dc29625cac38594e92
Ilya Zuyev <izuyev@google.com>
2020-10-20 17:17:49 -0700

    fix build script

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ env
 echo "============================="
 
 curl -LO https://github.com/kubernetes/minikube/releases/download/v1.14.0/minikube-darwin-amd64
-local -r MK=./minikube-darwin-amd64 
+MK=./minikube-darwin-amd64 
 chmod +x ${MK}
 ${MK} start --driver=docker
 ${MK} status
